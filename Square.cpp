5689c496c1714a288849e7caa546ab1625139ad0
whoxer <natanaelvsant@gmail.com>
2024-03-27 17:01:28 -0300

    Método init() que inicia o quadrado adicionada;  Método Area() para cálculo da área de um quadrado adicionado

--- Square.cpp
+++ Square.cpp
@@ -1,4 +1,26 @@
 #include "Square.hpp"
 
-Square::Square(/* args*/){}
+#include <iostream>
+
+Square::Square(){}
+
+void Square::init(float a, float b, float c, float d) {
+    side_a = a;
+    side_b = b;
+    side_c = c;
+    side_d = d;
+
+    if (side_a != side_b || side_b != side_c || side_c != side_d) {
+        std::cout << "all the square sides must have the same value." << std::endl;
+        errno;
+    } else {
+        std::cout << "Square of size: ";
+        std::cout << side_a << ":" << side_b << ":" << side_c << ":" << side_d;
+        std::cout << " created" << std::endl;
+    }   
+}
+double Square::Area() {
+    return side_a * side_a;
+}
+
 Square::~Square(){}
