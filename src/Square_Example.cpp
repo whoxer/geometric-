4ef142dff4cb195f4f43d634c23c67295dd004c5
whoxer <natanaelvsant@gmail.com>
2024-03-28 00:34:48 -0300

    Mudança de nome do arquivo

---
+++ src/Square_Example.cpp
@@ -0,0 +1,14 @@
+#include "Geometricpp.hpp"
+
+
+int main(void) {
+    Geometricpp::Square quadrado(100);
+
+    float area = quadrado.area();
+    float perimeter = quadrado.perimeter();
+    double diagonal = quadrado.diagonal();
+
+    std::cout << "Area: " << area << std::endl;
+    std::cout << "Perimeter: " << perimeter << std::endl;
+    std::cout << "Diagonal: " << diagonal << std::endl;
+}
