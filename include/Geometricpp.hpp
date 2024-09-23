a0d8cde1fc671b3d2f4167531b8335b77ee434e3
whoxer <natanaelvsant@gmail.com>
2024-09-22 22:17:54 -0300

    Biblioteca stdexcept; Classes declaradas em namespaces

--- include/Geometricpp.hpp
+++ include/Geometricpp.hpp
@@ -4,31 +4,24 @@
 
 #include <iostream>
 #include <cmath>
-
+#include <stdexcept>
 
 typedef double GEOMETRIC_SIDE;
 
 namespace Geometricpp {
-    class Square {
-        private:
-            GEOMETRIC_SIDE side_length;
-        public:
-            Square(GEOMETRIC_SIDE side_length) : side_length(side_length) {};
-
-            float size(){return side_length;}
-
-            double area() const {return pow(side_length, 2);}
-            double perimeter() const {return side_length * 4;}
-            double diagonal() const {return std::sqrt(2) * side_length;}
+    namespace Quadrilaterals {
+        class Square;
+        class Rectangle;
     };
 
-    class Triangle {
-        private:
-            GEOMETRIC_SIDE a, b, c;
-        public:
-            Triangle(GEOMETRIC_SIDE a, GEOMETRIC_SIDE b, GEOMETRIC_SIDE c) : a(a), b(b), c(c) {}
+
+    // class Triangle {
+    //     private:
+    //         GEOMETRIC_SIDE a, b, c;
+    //     public:
+    //         Triangle(GEOMETRIC_SIDE a, GEOMETRIC_SIDE b, GEOMETRIC_SIDE c) : a(a), b(b), c(c) {}
             
-    };
+    // };
     
 }
 
