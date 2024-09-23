f53061cafa12f5fce7a8709e47be56eb4002b204
whoxer <natanaelvsant@gmail.com>
2024-09-23 00:59:02 -0300

    Métodos do retângulo

--- src/Quadrilaterals.cpp
+++ src/Quadrilaterals.cpp
@@ -2,19 +2,36 @@
 
 
 // Definição dos métodos do Quadrado.
-double_t Quadrilaterals::Square::size() const {
+double_t Geometricpp::Quadrilaterals::Square::size() const {
     return side_length;
 }
 
-double_t Quadrilaterals::Square::area() const {
+double_t Geometricpp::Quadrilaterals::Square::area() const {
     return side_length * side_length;
 }
 
-double_t Quadrilaterals::Square::perimeter() const {
+double_t Geometricpp::Quadrilaterals::Square::perimeter() const {
     return side_length * 4;
 }
-double_t Quadrilaterals::Square::diagonal() const {
+double_t Geometricpp::Quadrilaterals::Square::diagonal() const {
     return std::sqrt(2) * side_length;
 }
 
 // Definição dos métodos do Retângulo
+
+std::pair<Geometricpp::GEOMETRIC_SIDE, Geometricpp::GEOMETRIC_SIDE> 
+Geometricpp::Quadrilaterals::Rectangle::size() const {
+    return std::make_pair(h, w);
+}
+
+double_t Geometricpp::Quadrilaterals::Rectangle::area() const {
+    return h * w;
+}
+
+double_t Geometricpp::Quadrilaterals::Rectangle::perimeter() const {
+    return 2 * (h + w);
+}
+
+double_t Geometricpp::Quadrilaterals::Rectangle::diagonal() const {
+    return sqrt(pow(h, 2) + pow(w, 2));
+}
