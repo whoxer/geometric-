84a47a7547385d34d491af30d9ce724b3a952c9c
whoxer <natanaelvsant@gmail.com>
2024-11-07 17:05:26 -0300

    Exemplo do teorema de pitágoras

---
+++ examples/Teoremas/pitagoras.cpp
@@ -0,0 +1,25 @@
+#include <Geometricpp.hpp>
+
+using namespace Geometricpp;
+
+int main(void)
+{
+    float_t x, y, z;
+
+    std::cout << "Geomtricpp v0.0.1.0" << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "Insira o comprimento do primeiro cateto: "
+    std::cin >> x;
+
+    std::cout << "Insira o comprimento do segundo cateto: " 
+    std::cin >> y;
+
+    z = pitagoras(x, y);
+
+    std::cout << "Cateto x: " << x << std::endl;
+    std::cout << "Cateto y: " << y << std::endl;
+    std::cout << "Hipotenusa z: " << z << std::endl;
+
+    return 0;
+}
